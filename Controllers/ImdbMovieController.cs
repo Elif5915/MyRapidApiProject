dc5d9ed70c1bfe9591a179a8f4c22318badd536a
Elif Yıldırım <elifyildirim3@outlook.com>
2024-12-19 23:29:39 +0300

    Bu projemde ImdbMovieController ındex view oluşturamıyorum çünkü pc deki virüs programı engelliyor. View oluşturup modeldeki verileri table haline getirip çalıştırırsak rapidapi servisinden verileri listelenir. Bu kısmı ne yazıkki kapanmayan virüs programımdan dolayı tamamlayamadım. Zaten sonra proje bitityordu. Ders29

--- Controllers/ImdbMovieController.cs
+++ Controllers/ImdbMovieController.cs
@@ -5,26 +5,26 @@ using Newtonsoft.Json;
 namespace MyRapidApiProject.Controllers;
 public class ImdbMovieController : Controller
 {
-    public async Task<IActionResult> Index()
-    {
+	public async Task<IActionResult> Index()
+	{
 
-        var client = new HttpClient();
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
-            Headers =
-    {
-        { "x-rapidapi-key", "8be6f92745mshaf73e9b2522c8f1p11a194jsn36e6ac18ef3b" },
-        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
-    },
-        };
-        using (var response = await client.SendAsync(request))
-        {
-            response.EnsureSuccessStatusCode();
-            var body = await response.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<List<ImdbMovieModel>>(body);
-            return View(values.ToList());
-        }
-    }
+		var client = new HttpClient();
+		var request = new HttpRequestMessage
+		{
+			Method = HttpMethod.Get,
+			RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+			Headers =
+	{
+		{ "x-rapidapi-key", "8be6f92745mshaf73e9b2522c8f1p11a194jsn36e6ac18ef3b" },
+		{ "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+	},
+		};
+		using (var response = await client.SendAsync(request))
+		{
+			response.EnsureSuccessStatusCode();
+			var body = await response.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ImdbMovieModel>>(body);
+			return View(values.ToList());
+		}
+	}
 }
